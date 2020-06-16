579d0e5dc2a42ac1f3f16ec2ef75c7a8dac8cc14
João Victor <jvc.martins12@gmail.com>
2020-06-16 15:14:33 -0300

    Usando Hibernate ORM Panache - Alteração do sufixo DAO para Repository

--- src/main/java/br/com/alura/resource/UserResource.java
+++ src/main/java/br/com/alura/resource/UserResource.java
@@ -3,6 +3,7 @@ package br.com.alura.resource;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,25 +12,26 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import br.com.alura.dao.UserDao;
+import br.com.alura.dao.UserRepository;
 import br.com.alura.model.User;
 
 @Path("/user")
 public class UserResource {
 
 	@Inject
-	UserDao dao;
+	UserRepository repository;
 
 	@POST
+	@Transactional
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response save(User user) {
-		dao.save(user);
+	public Response persist(User user) {
+		repository.persist(user);
 		return Response.status(201).build();
 	}
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<User> getAll() {
-		return dao.getAll();
+	public List<User> listAll() {
+		return repository.listAll();
 	}
 }
